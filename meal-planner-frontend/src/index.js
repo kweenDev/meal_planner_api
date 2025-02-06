b04893ccfa18876ff309b65fa810a0fdb932148c
KweenDev <refiloe15radebe@gmail.com>
2025-02-07 01:32:03 +0200

    chore(index.js): Set up React application entry point and import Bootstrap CSS for responsive design by Refiloe Radebe

---
+++ meal-planner-frontend/src/index.js
@@ -0,0 +1,14 @@
+/**
+ * index.js
+ * Author: Refiloe Radebe
+ * Date: 2025-02-06
+ * Description: Entry point for the React application.
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
